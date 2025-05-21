71cd8c4770d5d38b91c15bd893650cd94d405d53
Deepak <deepak.reddy45516@gmail.com>
2025-05-21 18:44:19 +0530

    Added TimelineSection

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
+import TimelineSection from '../components/TimelineSection';
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <HeroSection />
       <AboutSection />
+      <TimelineSection />
     </div>
   );
 };
