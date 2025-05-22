6860473c5c3b0677019d2d184dd80cf28307eb27
Deepak <deepak.reddy45516@gmail.com>
2025-05-22 17:04:03 +0530

    feat; added scroll to top on page mount

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import TimelineSection from '../components/TimelineSection';
 
 const HomePage: React.FC = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <HeroSection />
